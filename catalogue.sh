715c2ee0f231f510953178dc13d98442e78f9cf5
suhas yewale <suhasy983@gmail.com>
2024-07-19 15:17:58 +0530

    update

--- catalogue.sh
+++ catalogue.sh
@@ -1,22 +1,6 @@
 source common.sh
-cp catalogue.service /etc/systemd/system/catalogue.service
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-useradd roboshop
+component=catalogue
+app_path=/app
+schema_setup=mongo
 
 NODEJS
-
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
-
-cd /app
-unzip /tmp/catalogue.zip
-
-cd /app
-npm install
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
-
-dnf install mongodb-mongosh -y
-
-mongosh --host localhost </app/db/master-data.js
