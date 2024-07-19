d5cdc6a24c4aeebefa5fe805c88ed010212912b5
suhas yewale <suhasy983@gmail.com>
2024-07-19 17:50:18 +0530

    update

--- user.sh
+++ user.sh
@@ -1,10 +1,5 @@
-cp user.service  /etc/systemd/system/user.service
-mkdir /app
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
-cd /app
-unzip /tmp/user.zip
-cd /app
-npm install
-systemctl daemon-reload
-systemctl enable user
-systemctl start user
+source common.sh
+component=user
+app_path=/app
+
+NODEJS
